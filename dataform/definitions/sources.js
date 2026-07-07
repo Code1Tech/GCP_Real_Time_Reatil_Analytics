8882a5c214c1457355dbc45b84f14b049d8830f6
Code1Tech <Cloud@code1.dev>
2026-07-07 19:17:55 +0530

    Added Dataform style bronze silver gold transformations

---
+++ dataform/definitions/sources.js
@@ -0,0 +1,35 @@
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "sales_raw_enterprise"
+});
+
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "customers_raw"
+});
+
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "products_raw"
+});
+
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "stores_raw"
+});
+
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "returns_raw"
+});
+
+declare({
+  database: "gcp-project-usecase",
+  schema: "retail_bronze",
+  name: "inventory_raw_enterprise"
+});
